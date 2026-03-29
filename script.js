db1e5fb39f6373273f3087ab7edc44d16535ba88
joseroberto <joserobertoapd@gmail.com>
2026-03-29 18:29:26 -0300

    Implwmwnta estrutura da tela de jogo e lógica de sorteio de cores

--- script.js
+++ script.js
@@ -1,3 +1,42 @@
+const coresPrimarias = ["blue","yellow","red"];
+const cor1 = document.getElementById('cor1')
+const cor2 = document.getElementById('cor2')
+
+
+function sortearCor(){
+    let indice = Math.floor(Math.random()*coresPrimarias.length)
+    return coresPrimarias[indice]
+}
+
+function gerarCores(){
+
+    let cor1 = sortearCor();
+    let cor2 = sortearCor();
+
+    while (cor2 === cor1) {
+        cor2 = sortearCor();
+    }
+
+    return [cor1,cor2]
+
+}
+
+
+function misturarCores(cor1, cor2) {
+    const mistura = {
+        "blue-yellow": "green",
+        "blue-red": "purple",
+        "red-yellow": "orange"
+    };
+
+    let cores = [cor1, cor2];
+    cores.sort();
+
+    let chave = cores[0] + "-" + cores[1];
+
+    return mistura[chave];
+}
+
 function iniciarjogo(){
     console.log('botão clicado');
     const menu = document.getElementById('menu');
@@ -5,4 +44,15 @@ function iniciarjogo(){
 
     const jogo = document.getElementById('jogo');
     jogo.style.display = 'flex';
+
+
+    let [corA, corB] = gerarCores();
+
+    cor1.style.backgroundColor = corA;
+    cor2.style.backgroundColor = corB;
+
+    let resultado = misturarCores(corA, corB);
+
+    console.log("Resultado:", resultado);
 }
+
