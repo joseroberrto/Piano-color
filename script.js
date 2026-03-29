518006bb72d183009edb4e6fbf98c0a659d4475f
Luis Felipe <mendesmel420@gmail.com>
2026-03-29 15:59:00 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function iniciarjogo(){
+    console.log('botão clicado');
+    const menu = document.getElementById('menu');
+    menu.style.display = 'none';
+
+    const jogo = document.getElementById('jogo');
+    jogo.style.display = 'flex';
+}
